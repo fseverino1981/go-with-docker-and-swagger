fd3afc1572beab9bfd21b02ca155f5839f7f55f9
fseverino1981 <fasvb6@gmail.com>
2024-07-15 17:51:06 -0300

    Criado construtor da conexxão com banco, criada variaveis de ambiente para string de conexão e banco

--- src/configuration/database/mongodb/mongodb_connection.go
+++ src/configuration/database/mongodb/mongodb_connection.go
@@ -2,21 +2,22 @@ package mongodb
 
 import (
 	"context"
-	"go-with-docker-and-swagger/src/configuration/logger"
+	"os"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func InitConnection() {
-	ctx := context.Background()
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+func NewMongoDBConnection(ctx context.Context) (*mongo.Database, error) {
+	mongodb_uri := os.Getenv("MONGODB_URL")
+	mongodb_database := os.Getenv("MONGODB_USER_DB")
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongodb_uri))
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 	if err = client.Ping(ctx, nil); err != nil {
-		panic(err)
+		return nil, err
 	}
 
-	logger.Info("Conseguiu conectar")
+	return client.Database(mongodb_database), nil
 }
