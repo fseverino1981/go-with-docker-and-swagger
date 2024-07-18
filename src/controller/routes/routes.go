6d032f70acbc951898e887aef45dad812d624e0a
fseverino1981 <fasvb6@gmail.com>
2024-07-18 14:53:22 -0300

    Implementada a função de validação de token pelo meddleware, adicionada essa funçao no handlefunc nos  metodos de rotas

--- src/controller/routes/routes.go
+++ src/controller/routes/routes.go
@@ -2,17 +2,18 @@ package routes
 
 import (
 	"go-with-docker-and-swagger/src/controller"
+	"go-with-docker-and-swagger/src/model"
 
 	"github.com/gin-gonic/gin"
 )
 
 func InitRoutes(r *gin.RouterGroup, userController controller.UserControllerInterface) {
 
-	r.GET("/getUserById/:userId", userController.FindUserById)
-	r.GET("/getUserByEmail/:userEmail", userController.FindUserByEmail)
-	r.POST("/createUser", userController.CreateUser)
-	r.PUT("/updateUser/:userId", userController.UpdateUser)
-	r.DELETE("/deleteUser/:userId", userController.DeleteUser)
+	r.GET("/getUserById/:userId", model.VerifTokenMiddleware, userController.FindUserById)
+	r.GET("/getUserByEmail/:userEmail", model.VerifTokenMiddleware, userController.FindUserByEmail)
+	r.POST("/createUser", model.VerifTokenMiddleware, userController.CreateUser)
+	r.PUT("/updateUser/:userId", model.VerifTokenMiddleware, userController.UpdateUser)
+	r.DELETE("/deleteUser/:userId", model.VerifTokenMiddleware, userController.DeleteUser)
 
 	r.POST("/login", userController.LoginUser)
 
